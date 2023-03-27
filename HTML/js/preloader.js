ad3bda703e426b6f04c407621cb82827fb18d242
LayzaLi <90182280+LayzaLi@users.noreply.github.com>
2023-03-27 20:11:17 +0300

    Delete HTML directory

--- HTML/js/preloader.js
+++
@@ -1,7 +0,0 @@
-document.body.onload = function() {
-
-	var preloader = document.getElementById('page-preloader');
-		if(!preloader.classList.contains('done')){
-			preloader.classList.add('done')
-		}
-}
